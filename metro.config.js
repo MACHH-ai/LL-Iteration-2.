1b88f3541c1c6b0cab5ca64f56f8978a17e05253
MACHH-ai <jacksonsessil@gmail.com>
2025-07-07 22:40:21 +0300

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.resolveRequest = require('metro-resolver-symlinks').create  ({ // or `createReactNativeResolver`
-  get  (context, moduleName, platform) {
-    if (moduleName.startsWith('@/')) {
-      return context.resolveRequest(context, moduleName.replace(/^@\//, './'), platform);
-    }
-    return context.resolveRequest(context, moduleName, platform);
-  },
-});
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.startsWith('@/')) {
+    return context.resolveRequest(context, path.join(context.projectRoot, moduleName.replace('@/', '')), platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
